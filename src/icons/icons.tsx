a3f08d07844984403820a6a6cce9578baff6ccb5
Josue Ferreyra <josueferreyra8824@gmail.com>
2023-10-04 21:02:32 -0300

    ADDED: Icons from Tabler-Icons

---
+++ src/icons/icons.tsx
@@ -0,0 +1,48 @@
+export const ListIcon = ({ className } : { className?: string}) => (
+  <svg className={className} width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M13 5h8' />
+    <path d='M13 9h5' />
+    <path d='M13 15h8' />
+    <path d='M13 19h5' />
+    <path d='M3 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z' />
+    <path d='M3 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z' />
+  </svg>
+)
+export const SquareCheck = ({ className } : { className?: string}) => (
+  <svg className={className} width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M18.333 2c1.96 0 3.56 1.537 3.662 3.472l.005 .195v12.666c0 1.96 -1.537 3.56 -3.472 3.662l-.195 .005h-12.666a3.667 3.667 0 0 1 -3.662 -3.472l-.005 -.195v-12.666c0 -1.96 1.537 -3.56 3.472 -3.662l.195 -.005h12.666zm-2.626 7.293a1 1 0 0 0 -1.414 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z' strokeWidth='0' fill='currentColor' />
+  </svg>
+)
+export const ToolsKitchen = ({ className } : { className?: string}) => (
+  <svg className={className} width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M19 3v12h-5c-.023 -3.681 .184 -7.406 5 -12zm0 12v6h-1v-3m-10 -14v17m-3 -17v3a3 3 0 1 0 6 0v-3' />
+  </svg>
+)
+export const MoodOk = ({ className } : { className?: string}) => (
+  <svg className={className} width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M20.925 13.163a8.998 8.998 0 0 0 -8.925 -10.163a9 9 0 0 0 0 18' />
+    <path d='M9 10h.01' />
+    <path d='M15 10h.01' />
+    <path d='M9.5 15c.658 .64 1.56 1 2.5 1s1.842 -.36 2.5 -1' />
+    <path d='M15 19l2 2l4 -4' />
+  </svg>
+)
+export const ExternalLink = ({ className } : { className?: string}) => (
+  <svg className={className} width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6' />
+    <path d='M11 13l9 -9' />
+    <path d='M15 4h5v5' />
+  </svg>
+)
+export const ChevronDown = ({ className } : { className?: string}) => (
+  <svg className={className} width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M7 7l5 5l5 -5' />
+    <path d='M7 13l5 5l5 -5' />
+  </svg>
+)
