19ca1c7186795cc8dca2efdca6636e8d3ca0b7a7
Josue Ferreyra <josueferreyra8824@gmail.com>
2023-10-04 21:05:41 -0300

    CREATED: Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import { useState, type FormEvent } from 'react'
+import { useRouter } from 'next/navigation'
+import { ChevronDown } from '@/icons/icons'
+import styles from './page.module.css'
+
+export default function Search () {
+  const [showFilters, setShowFilters] = useState(false)
+  const router = useRouter()
+
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target as HTMLFormElement)
+
+    const searchParams = new URLSearchParams()
+    formData.forEach((value, key) => {
+      if (value) {
+        searchParams.append(key, value as string)
+      }
+    })
+
+    const url = `/recipes?${searchParams}`
+    router.push(url)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className='bg-zinc-950 relative isolate'>
+      {/* Background */}
+      <div className={`${styles.moveBackground} mainBackground inset-0 absolute -z-10`} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Content */}
+      <article className='max-w-4xl mx-auto p-3 grid place-content-center h-full'>
+        <h1 className='text-6xl text-center font-bold my-12'>The Recipe Finder</h1>
+        <form className='flex flex-col gap-4' action='/recipes' onSubmit={handleSearch}>
+          <fieldset>
+            <legend className='hidden'>Dish Name</legend>
+            <label className='hidden' htmlFor='dishName'>Dish Name</label>
+            <input className='w-full rounded p-3' id='dishName' name='q' type='text' placeholder='Dish Name' />
+          </fieldset>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <button type='button' className='text-sm flex justify-between items-center p-3 bg-rose-900 rounded' onClick={() => setShowFilters(!showFilters)}>
+            <ChevronDown className={`${showFilters ? 'rotate-180' : ''} transition-transform`} /> {showFilters ? 'Hide' : 'Show'} Additional filters <ChevronDown className={`${showFilters ? 'rotate-180' : ''} transition-transform`} />
+          </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {
+            showFilters
+              ? (
+                <fieldset className='flex flex-col gap-3'>
+                  <fieldset className='flex flex-col text-center gap-1'>
+                    <legend className='hidden'>Meal Type</legend>
+                    <label className='text-sm' htmlFor='mealType'>Meal Type</label>
+                    <select id='mealType' name='mealType' className='p-2 rounded' placeholder='Meal Type' defaultValue=''>
+                      <option value=''>Any</option>
+                      <option value='breakfast'>Breakfast</option>
+                      <option value='dinner'>Dinner</option>
+                      <option value='lunch'>Lunch</option>
+                      <option value='snack'>Snack</option>
+                      <option value='teatime'>Teatime</option>
+                    </select>
+                  </fieldset>
+                  <fieldset className='flex flex-col text-center gap-1'>
+                    <legend className='hidden'>Diet</legend>
+                    <label className='text-sm' htmlFor='diet'>Diet</label>
+                    <select id='diet' name='diet' className='p-2 rounded' placeholder='Diet' defaultValue=''>
+                      <option value=''>Any</option>
+                      <option value='balanced'>Balanced</option>
+                      <option value='high-fiber'>High Fiber</option>
+                      <option value='high-protein'>High Protein</option>
+                      <option value='low-carb'>Low Carb</option>
+                      <option value='low-fat'>Low Fat</option>
+                      <option value='low-sodium'>Low Sodium</option>
+                    </select>
+                  </fieldset>
+                  <fieldset className='flex flex-col text-center gap-1'>
+                    <legend className='hidden'>Dietary Considerations</legend>
+                    <label className='text-sm' htmlFor='dietaryConsiderations'>Dietary Considerations</label>
+                    <select className='p-2 rounded' name='health' id='dietaryConsiderations' defaultValue=''>
+                      <option value=''>None</option>
+                      <option value='alcohol-cocktail'>Alcohol Cocktail</option>
+                      <option value='alcohol-free'>Alcohol Free</option>
+                      <option value='celery-free'>Celery Free</option>
+                      <option value='crustacean-free'>Crustacean Free</option>
+                      <option value='dairy-free'>Dairy Free</option>
+                      <option value='DASH'>DASH</option>
+                      <option value='egg-free'>Egg Free</option>
+                      <option value='fish-free'>Fish Free</option>
+                      <option value='fodmap-free'>Fodmap Free</option>
+                      <option value='gluten-free'>Gluten Free</option>
+                      <option value='immuno-supportive'>Immuno Supportive</option>
+                      <option value='keto-friendly'>Keto Friendly</option>
+                      <option value='kidney-friendly'>Kidney Friendly</option>
+                      <option value='kosher'>Kosher</option>
+                      <option value='low-fat-abs'>Low Fat ABS</option>
+                      <option value='low-potassium'>Low Potassium</option>
+                      <option value='low-sugar'>Low Sugar</option>
+                      <option value='lupine-free'>Lupine Free</option>
+                      <option value='Mediterranean'>Mediterranean</option>
+                      <option value='mollusk-free'>Mollusk Free</option>
+                      <option value='mustard-free'>Mustard Free</option>
+                      <option value='no-oil-added'>No Oil Added</option>
+                      <option value='paleo'>Paleo</option>
+                      <option value='peanut-free'>Peanut Free</option>
+                      <option value='pescatarian'>Pescatarian</option>
+                      <option value='pork-free'>Pork Free</option>
+                      <option value='red-meat-free'>Red Meat Free</option>
+                      <option value='sesame-free'>Sesame Free</option>
+                      <option value='shellfish-free'>Shellfish Free</option>
+                      <option value='soy-free'>Soy Free</option>
+                      <option value='sugar-conscious'>Sugar Conscious</option>
+                      <option value='sulfite-free'>Sulfite Free</option>
+                      <option value='tree-nut-free'>Tree Nut Free</option>
+                      <option value='vegan'>Vegan</option>
+                      <option value='vegetarian'>Vegetarian</option>
+                      <option value='wheat-free'>Wheat Free</option>
+                    </select>
+                  </fieldset>
+                </fieldset>
+                )
+              : <></>
+          }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <button className='mt-1 bg-rose-700 p-4 rounded text-lg font-bold' type='submit'>Seach 🔎</button>
+        </form>
+      </article>
     </main>
   )
 }
